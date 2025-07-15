83155f485bb1dc822b46e37b51b7e1399ce95bb7
coderanay <147708257+coderanay@users.noreply.github.com>
2025-07-15 11:27:38 +0530

    Time: 4 ms (35.54%) | Memory: 42.6 MB (48.66%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxFreqSum(String s) {
+        HashMap<Character, Integer> freq = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+        int maxVowelFreq = 0;
+        int maxConsonantFreq = 0;
+        for (char c : freq.keySet()) {
+            int count = freq.get(c);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                maxVowelFreq = Math.max(maxVowelFreq, count);
+            } else if (Character.isLetter(c)) { 
+                maxConsonantFreq = Math.max(maxConsonantFreq, count);
+            }
+        }
+
+        return maxVowelFreq + maxConsonantFreq;
+    }
+}
