cccc14d41ba55d38ed7921704102803323c5e342
coderanay <147708257+coderanay@users.noreply.github.com>
2025-09-11 03:01:11 +0530

    Time: 277 ms (20.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,11 @@
+SELECT
+    s.machine_id,
+    ROUND(AVG(e.timestamp - s.timestamp), 3) AS processing_time
+FROM activity s
+JOIN activity e
+  ON s.machine_id = e.machine_id
+ AND s.process_id = e.process_id
+ AND s.activity_type = 'start'
+ AND e.activity_type = 'end'
+ AND e.timestamp >= s.timestamp
+GROUP BY s.machine_id;
