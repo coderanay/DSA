fa81c65435e63ca91fc923f4f4bc7f17a9c2b14c
coderanay <147708257+coderanay@users.noreply.github.com>
2025-10-03 16:45:58 +0530

    Time: 5 ms (26.15%) | Memory: 55.7 MB (53.12%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -5,9 +5,10 @@ class Solution {
         if(m * n != original.length) return new int[0][0];
         for(int i=0;i<m;i++)
         {
-            for(int j=0;j<n && idx<original.length;j++)
+            for(int j=0;j<n ;j++)
             {
-                d[i][j] = original[idx++];
+                if(idx<original.length)
+                    d[i][j] = original[idx++];
             }
         }
         return d;
