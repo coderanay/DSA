b13dacbcc05f73e9a57e1e45e9220b40e130647d
coderanay <147708257+coderanay@users.noreply.github.com>
2025-10-05 12:01:08 +0530

    Time: 12 ms (1.76%) | Memory: 40.9 MB (11.34%) - LeetSync

---
+++ 1769-get-maximum-in-generated-array/get-maximum-in-generated-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int getMaximumGenerated(int n) {
+        if(n==0) return 0;
+        int[] arr = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i=1;i<=n;i++)
+        {
+             if(2*i <= n)arr[2 * i] = arr[i];
+             if(2*i+1 <= n)arr[2 * i + 1] = arr[i] + arr[i + 1];
+        }
+        int max=0;
+        for(int x: arr) 
+        {
+            max = Math.max(max,x);
+            System.out.println(x);
+        }
+        return max;
+    }
+}
