f5e66ccc9aaefefda9559da8fde6b58fcf204fe3
coderanay <147708257+coderanay@users.noreply.github.com>
2025-11-05 22:02:47 +0530

    Time: 6 ms (48.50%) | Memory: 48.7 MB (8.71%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,9 +1,60 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i=0;i<nums.length;i++) {
-            nums[i] = nums[i] * nums[i];
+        int n = nums.length;
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] < 0)
+            {
+                a.add(nums[i]);
+            } else b.add(nums[i]);
         }
-        Arrays.sort(nums);
+        if(a.size()== 0)
+        {
+            for(int i=0;i<b.size();i++)
+            {
+                b.set(i,b.get(i)*b.get(i));
+            }
+            return b.stream().mapToInt(k->k).toArray();
+        } else if(b.size() == 0)
+        {
+            for(int i=0;i<a.size();i++)
+            {
+               a.set(i,a.get(i)*a.get(i));
+            }
+            Collections.reverse(a);
+            return a.stream().mapToInt(k->k).toArray();
+        }
+        for(int i=0;i<a.size();i++) 
+        {
+            a.set(i,a.get(i)*a.get(i));
+        }
+        Collections.reverse(a);
+        for(int i=0;i<b.size();i++) b.set(i,b.get(i)*b.get(i));
+        int i=0,j=0,idx=0;
+        while(i < a.size() && j < b.size())
+        {
+            if(a.get(i) <= b.get(j))
+            {
+                nums[idx++] = a.get(i);
+                i++;
+            } else if(b.get(j) <= a.get(i))
+            {
+                nums[idx++] = b.get(j);
+                j++;
+            }
+        }
+        while(i < a.size())
+        {
+            nums[idx++] = a.get(i);
+            i++;
+        }
+        while(j < b.size())
+        {
+            nums[idx++] = b.get(j);
+            j++;
+        }
         return nums;
     }
 }
