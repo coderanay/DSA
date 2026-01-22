f92049d546b70a2de7e32e92992df34ad347f531
Vinay Vishwakarma <147708257+coderanay@users.noreply.github.com>
2026-01-22 16:50:49 +0530

    Time: 4 ms (17.93%) | Memory: 44.9 MB (83.00%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,19 +1,25 @@
 class Solution {
-    private void findSubsets(int index, int[] nums, List<List<Integer>> ans, List<Integer> ds) {
-        ans.add(new ArrayList<>(ds)); 
-        for (int i = index; i < nums.length; i++) {
-            if (i > index && nums[i] == nums[i - 1]) continue;
 
-            ds.add(nums[i]);
-            findSubsets(i + 1, nums, ans, ds);
-            ds.remove(ds.size() - 1); 
+    public void backtrack(Set<List<Integer>> set, int[] nums, int i, List<Integer> temp) {
+        if (i == nums.length) {
+            List<Integer> copy = new ArrayList<>(temp);
+            Collections.sort(copy);
+            set.add(copy);
+            return;
         }
+        temp.add(nums[i]);
+        backtrack(set, nums, i + 1, temp);
+        temp.remove(temp.size() - 1);
+        backtrack(set, nums, i + 1, temp);
     }
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        findSubsets(0, nums, ans, new ArrayList<>());
-        return ans;
+
+        Set<List<Integer>> set = new HashSet<>();
+        List<Integer> temp = new ArrayList<>();
+
+        backtrack(set, nums, 0, temp);
+
+        return new ArrayList<>(set);
     }
 }
